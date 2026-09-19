6596f97a4b798fd1b02de00b88f1efefb893759c
Benyamin Sojdeh ei <Benyaminsojdehei1383@gmail.com>
2026-09-19 23:37:51 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+const menuButton = document.getElementById("menu-btn");
+const nav = document.getElementById("nav");
+
+menuButton.addEventListener("click", () => {
+    nav.classList.toggle("open");
+});
+
+
+document.querySelectorAll("#nav a").forEach(link => {
+
+    link.addEventListener("click", () => {
+        nav.classList.remove("open");
+    });
+
+});
+
+
+const observer = new IntersectionObserver(
+    (entries) => {
+
+        entries.forEach(entry => {
+
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            }
+
+        });
+
+    },
+    {
+        threshold: 0.12
+    }
+);
+
+
+document.querySelectorAll(".reveal").forEach(element => {
+    observer.observe(element);
+});
